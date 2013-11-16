fe81cb4b7c49406ce3b83a7bfbebd4c617387779
César D. Rodas <crodas@php.net>
2013-11-16 00:43:47 -0300

    added examples

---
+++ example/apps/foo.php
@@ -0,0 +1,14 @@
+<?php
+
+/** 
+ *  @Cli("foobar", "foobar this is foobar") 
+ *  @Arg('name', OPTIONAL, 'add name')
+ *  @Option('foobar', VALUE_REQUIRED|VALUE_IS_ARRAY, 'add name')
+ */
+function cli_foo($input, $output)
+{
+    $arg = $input->getArgument('name');
+    $opt = $input->getOption('foobar');
+
+    $output->writeLn(json_encode(compact('arg', 'opt')));
+}
---
+++ example/cli.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$cli = new crodas\cli\Cli(".tmp");
+$cli->addDirectory(__DIR__ . '/../vendor');
+$cli->addDirectory(__DIR__ . '/apps');
+$cli->main();
